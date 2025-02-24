ce3837394d251e7116fc5aeed182f1e0d92212d2
Harikrishna <Harikrishna305@gmail.com>
2025-02-24 18:19:38 +0530

    first commit

---
+++ gas_utilit_yproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gas_utilit_yproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gas_utilit_yproject.settings')
+
+application = get_asgi_application()
